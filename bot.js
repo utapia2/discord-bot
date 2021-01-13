cd069f101d957a4d6c1b3b349ab61690b78181d7
Tapia <utapia@scuc.txed.net>
2021-01-13 11:30:30 -0600

    removed old code

--- bot.js
+++ bot.js
@@ -69,26 +69,4 @@ client.on('message', msg => {
 
 
 })
-
-
-
-
-/*
-
-the old code
-client.on('message', message =>{
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'ping'){
-        message.channel.send('pong!');
-    }
-    if (command == 'youtube'){
-        message.channel.send('youtube weeee');
-    }
-});
-*/
-
 client.login(token);
